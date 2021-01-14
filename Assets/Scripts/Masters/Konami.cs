650c6784fd421f621f81ced20debc8c6d489403a
John Paul Depew <jpdepewjp@gmail.com>
2021-01-13 21:34:42 -0500

    Konami code

--- Assets/Scripts/Masters/Konami.cs
+++ Assets/Scripts/Masters/Konami.cs
@@ -1,19 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Konami : MonoBehaviour
 {
     Data data = Data.Instance;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    KeyCode[] keyCodes = new KeyCode[] {
+        KeyCode.UpArrow,
+        KeyCode.UpArrow,
+        KeyCode.DownArrow,
+        KeyCode.DownArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.B,
+        KeyCode.A
+    };
+    int keyCodeIndex = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (keyCodeIndex >= keyCodes.Length)
+        {
+            // Konami surprise!!
+            print("Konami surpirse");
+            data.konamiEnabled = true;
+        }
+        else
+        {
+            if (Input.GetKeyDown(keyCodes[keyCodeIndex]))
+            {
+                keyCodeIndex++;
+            }
+            else if (Input.anyKeyDown)
+            {
+                keyCodeIndex = 0;
+            }
+        }
     }
 }
